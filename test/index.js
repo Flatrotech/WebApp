9436c791d160408afb2d81c7e33df72141c07f2c
Brendan Nason <flatrotech@gmail.com>
2021-06-14 23:06:47 -0500

    forgot to add back tests

---
+++ test/index.js
@@ -0,0 +1,16 @@
+/**
+ * @copyright Copyright 2021 Brendan Nason <flatrotech@gmail.com>
+ * @license MIT
+ */
+
+'use strict';
+
+const modulename = require('..');
+
+describe('modulename', () => {
+  it('does something', (done) => {
+    // Assert something about modulename here
+    modulename();
+    done();
+  });
+});
