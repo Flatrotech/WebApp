5b01d3fc0d26eda003349e512c99f185da13e338
Brendan Nason <flatrotech@gmail.com>
2021-06-14 22:56:42 -0500

    rearranged the repository and added an api backend solution that is supposed to use spotify at some point

---
+++ backend/WebApp.Api/WebApp.Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApp.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ backend/WebApp.Api/WebApp.DAL/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.DAL
+{
+    public class WebAppDbContext : DbContext
+    {
+        public DbSet<>
+
+        public WebAppDbContext() : base()
+        {
+
+        }
+
+        public WebAppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
